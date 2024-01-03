ec21544d87d2f5ce4b47a4804a128172a79241eb
Adrien Sauvage <94073350+AdriSauv@users.noreply.github.com>
2024-01-03 12:23:47 +0100

    création de la méthode getAttaque() de la classe Classe

--- src/main/java/fr/adsa/classes/Classe.java
+++ src/main/java/fr/adsa/classes/Classe.java
@@ -3,16 +3,16 @@ package fr.adsa.classes;
 import fr.adsa.actions.BasicAttaque;
 import fr.adsa.interfaces.IAttaque;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Classe {
     public String nom;
-    public List<IAttaque> attaques = new ArrayList<IAttaque>();
+    // liste d'attaque de la classe, qui comprend juste BasicAttaque pour l'instant
+    public List<IAttaque> attaques = List.of(new BasicAttaque(10));
 
-    public Classe(String nom){
+    public Classe(String nom, List<IAttaque> attaques) {
         this.nom = nom;
-        this.attaques.add(new BasicAttaque(10));
+        this.attaques = attaques;
     }
 
     public String getNom() {
@@ -23,8 +23,9 @@ public class Classe {
         this.nom = nom;
     }
 
-    public List<IAttaque> getAttaques() {
-        return attaques;
+    // recupere une attaque de la liste d'attaque aléatoirement
+    public IAttaque getAttaques() {
+        return attaques.get((int) (Math.random() * attaques.size()));
     }
 
     public void setAttaques(List<IAttaque> attaques) {
